52e86b40b13138c20f9cc90d8602799acbe3b9e7
Aluno <liviarsouza@gmail.com>
2018-07-21 09:38:35 -0300

    UI completed

---
+++ My Games/My Games/Controllers/AddEditViewController.swift
@@ -0,0 +1,49 @@
+//
+//  AddEditViewController.swift
+//  My Games
+//
+//  Created by Aluno on 21/07/18.
+//  Copyright © 2018 CESAR School. All rights reserved.
+//
+
+import UIKit
+
+class AddEditViewController: UIViewController {
+    
+    @IBOutlet weak var tfTitle: UITextField!
+    @IBOutlet weak var tfConsole: UITextField!
+    @IBOutlet weak var dpReleaseDate: UIDatePicker!
+    @IBOutlet weak var btAddEdit: UIButton!
+    @IBOutlet weak var btCover: UIButton!
+    @IBOutlet weak var ivCover: UIImageView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func AddEditCover(_ sender: UIButton) {
+        // para adicionar uma imagem da biblioteca
+    }
+    
+    @IBAction func addEditGame(_ sender: UIButton) {
+        // acao salvar novo ou editar existente
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
